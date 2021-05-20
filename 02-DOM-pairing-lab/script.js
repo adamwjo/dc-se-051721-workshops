da4b6cfbd035cc91e6a30475cb57c1e9813111e2
Adam Johnson <adamwjo@gmail.com>
2021-05-20 16:24:29 -0400

    todays workshop

---
+++ 02-DOM-pairing-lab/script.js
@@ -0,0 +1,38 @@
+
+
+// 1.) Write a function that will grab the title and change it to a string of your choosing
+// Don't forget to actually run your function
+
+
+
+// 2.) Write a function, that given a color, will change the background color of the table to that color
+
+
+
+
+// 3.) Write a function that will take in some values for: name, type, weight, image, and create a new row that displays
+// those values.
+// for the image use this ----> 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/REPLACE-ME-WITH-A-NUM-1-500.png'
+
+
+
+// 4.) Write a function that will delete the last row in the table
+
+
+
+
+/// ******* TIPS **************
+
+// - if the style isnt looking quite right make sure you are adding the appropriate class name.
+// - When creating the table row refer to this 
+
+//                     <tr>
+                    //     <th class="pokemon">Bulbasaur</th>
+                    //     <td>Grass</td>
+                    //     <td>12lbs</td>
+                    //     <td>
+                    //         <image
+                    //             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png">
+                    //         </image>
+                    //     </td>
+                    // </tr>
